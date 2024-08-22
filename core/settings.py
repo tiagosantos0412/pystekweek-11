7123e0e6b541bbd7bae3d9ba274dd5a7cdcd731f
Tiago <tiagosantos0412@gmail.com>
2024-08-22 10:41:35 -0300

    Configurando a segurança do banco de dados.

--- core/settings.py
+++ core/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='195.179.231.129'),
+        'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
     }
 }
